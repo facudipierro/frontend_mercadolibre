1a43932a2ae9b0ac8dc99a3bf25416bdceaeefe5
Facundo Di Pierro <facudipierro@gmail.com>
2017-02-21 16:59:34 -0300

    agrego gitignore

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// Required dependencies
 var Twig = require("twig");
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var api_url = 'https://api.mercadolibre.com';
 
+// Extending twig for style decimals 
 Twig.extendFunction("split_money", function(value) {
 	var price = value.toString().split(".")[0];
 	var decimals = value.toString().split(".")[1];
@@ -26,6 +29,7 @@ var nbVisits = 0;
 
 app.set('view engine', 'twig');
 
+// Render index
 app.get('/', function(req, res) {
     nbVisits++;
     res.render('index.twig', {
@@ -33,13 +37,15 @@ app.get('/', function(req, res) {
     });
 });
 
+// Search by word
 app.get('/items', urlencodedParser, function(req, res) {
 	var search_txt = req.query.search;
 	
-	request.get('https://api.mercadolibre.com/sites/MLA/search?q='+search_txt, function(err, response) {
+	request.get(api_url+'/sites/MLA/search?q='+search_txt, function(err, response) {
 		if (err) {
 			res.render('error.twig');
 		} else {
+			// Render search results
 			var respuesta = JSON.parse(response.body);
 			res.render('listado.twig', {
 				productos: respuesta.results.slice(0, 4)
@@ -48,16 +54,18 @@ app.get('/items', urlencodedParser, function(req, res) {
 	});
 });
 
+// Search by id_item
 app.get('/items/:id_producto', urlencodedParser, function(req, res) {
 	var idProducto = req.params.id_producto;
-	request.get("https://api.mercadolibre.com/items/"+idProducto, function(err, responseArticulo) {
+	request.get(api_url+"/items/"+idProducto, function(err, responseArticulo) {
 		if (err) {
 			res.render('error.twig');
 		} else {
-			request.get("https://api.mercadolibre.com/items/"+idProducto+"/description", function(err, responsePublicacion) {
+			request.get(api_url+"/items/"+idProducto+"/description", function(err, responsePublicacion) {
 				if (err) {
 					res.render("error.twig");
 				} else {
+					// Render product details
 					var articulo = JSON.parse(responseArticulo.body);
 					var publicacion = JSON.parse(responsePublicacion.body);
 					res.render("detalle.twig", {
@@ -70,6 +78,8 @@ app.get('/items/:id_producto', urlencodedParser, function(req, res) {
 	});
 });
 
+// Set route for assets
 app.use(express.static('assets'));
 
+// Start server
 app.listen(8080);
